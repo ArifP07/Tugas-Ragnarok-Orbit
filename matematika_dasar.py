b92349df31ee079785169996dcfe93ab9ffb23bb
ArifP07 <100903650+ArifP07@users.noreply.github.com>
2022-03-13 22:50:24 +0700

    Create matematika_dasar.py
    ini modulnya

---
+++ matematika_dasar.py
@@ -0,0 +1,16 @@
+from abc import abstractmethod, ABC
+
+def pertambahan(a, b):
+    return a+b
+
+def pengurangan(a, b):
+    return a-b
+
+def perkalian(a, b):
+    return a*b
+
+def pembagian(a, b):
+    return a/b
+
+def nama():
+    print("Arif Priambodo")
